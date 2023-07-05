86a3f04af1fcfe1872c7167bd329dda79ea37ff8
maple5717 <momiji5717@gmail.com>
2023-07-05 20:03:17 +0800

    updated pid control

--- src/pid/scripts/pid_helper_tacniq.py
+++ src/pid/scripts/pid_helper_tacniq.py
@@ -35,10 +35,11 @@ class PID_HELPER():
         #+0.035 # 80 - 60 # in terms of desired pressure 230, 80
         self.TOLERANCE = 10
         self.TOLERANCE_QTY = 10
+        self.TACNIQ_SCALE = 20
 
         self.input_topic = rospy.get_param("~input", "input")
         self.output_topic = rospy.get_param("~output", "output")
-        self.GOAL = rospy.get_param("~goal", 0.024*3)
+        self.GOAL = rospy.get_param("~goal", 0.024*3*0 + 0.1)
         self.state=0
         self.current_pos=0
         rospy.init_node('pid_helper')
@@ -56,7 +57,7 @@ class PID_HELPER():
         self.command.rPR = 0 # Desired target
         self.command.rSP = 0 # Desired speed: keep 0
         self.command.rFR = 0 # Desired force: keep 0
-        # print("aaaaaaaaaaaaaaa\naaaaaaaaaaaaaaaaaaaaaa")
+        
         self.init_gripper()
         self.pub_pid_start.publish(Bool(data=0))
         # start with msg
@@ -69,6 +70,7 @@ class PID_HELPER():
 
     def getStatus(self, status):
         self.current_pos = status.gPO
+        # self.updatePlant()
         #if self.current_pos >= self.GOAL:
         #    self.current_pos = self.GOAL
 
@@ -76,15 +78,18 @@ class PID_HELPER():
     def init_gripper(self):
         self.command.rACT = 0
         self.pub_plant.publish(self.command)
-        rospy.sleep(0.1)
+        rospy.sleep(0.2)
         self.command.rACT = 1
         self.command.rGTO = 1
+        self.command.rSP = 255
+        # self.command.rFR = 150
 
         self.pub_plant.publish(self.command)
+        self.command.rSP = 0
         print('Activated')
 
         # wait until open
-        rospy.sleep(2)
+        rospy.sleep(1.5)
 
         # send goal stuff
         self.pub_goal.publish(Float64(data=self.GOAL))
@@ -105,14 +110,14 @@ class PID_HELPER():
 
     def updatePlant(self,data):
         '''receive and send output of the pid controller to gripper'''
-        action = self.current_pos + int(data.data*200)
+        action = self.current_pos + int(data.data*self.TACNIQ_SCALE) 
         print('Input to the plant:', data.data)
         print('State: ', self.state)
         print('Error: ', self.GOAL - self.state)
         action = max(0, action)
         action = min(210, action)
         self.command.rPR = action
-        print('Pos + Action: ', self.current_pos, int(data.data*200), data.data)
+        print('Pos + Action: ', self.current_pos, int(data.data*self.TACNIQ_SCALE), data.data)
         print('control effort: ', action)
         # print(self.current_pos, data.data, self.command.rPR)
         self.pub_plant.publish(self.command)
@@ -138,8 +143,8 @@ class PID_HELPER():
             # combine left_data and right_data here and publish the result
             average_val = np.mean(left_image) + np.mean(right_image)
             average_force = 1 - average_val/ 2 / max_adc_value
-            self.pub.publish(5 * average_force-  0*0.020768) # publish the combined data 
-            self.state = 5 * average_force - 0*0.020768 # this is a temporary value
+            self.pub.publish(10*average_force) # publish the combined data 
+            self.state = 10*average_force # this is a temporary value (5 * average_force - 0*0.020768)
             left_updated = False  # reset the flag
             right_updated = False
             self.pub_pid_start.publish(Bool(data=1))
@@ -147,6 +152,7 @@ class PID_HELPER():
     def listener(self): 
         rospy.Subscriber('tacniq/left', Int16MultiArray, self.read_left_tacniq)
         rospy.Subscriber('tacniq/right', Int16MultiArray, self.read_right_tacniq)
+        # rospy.Subscriber('control_effort', Float64, self.updatePlant)
         rospy.Subscriber('control_effort', Float64, self.updatePlant)
         rospy.spin()
 
